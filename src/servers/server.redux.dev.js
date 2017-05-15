f237d3997a361c44e84a42175a758b4458f8e05d
Tomas Weiss <tomasw@salsitasoft.com>
2017-05-15 13:03:08 +0200

    redux dev client entry point

--- src/servers/server.redux.dev.js
+++ src/servers/server.redux.dev.js
@@ -3,12 +3,13 @@ import express from 'express';
 import enrichClientBundleWithHotReloading from './enrichClientBundleWithHotReloading';
 import enrichServerSideRenderingHandler from './enrichServerSideRenderingHandler';
 import exitWhenError from './exitWhenError';
+import htmlTemplate from '../htmlTemplate';
 import reactReduxProvider from './reactReduxProvider';
 import { resolveDevStackPath } from '../utils/pathResolvers';
 
 exitWhenError();
 
 const server = express();
-enrichClientBundleWithHotReloading(server, resolveDevStackPath('src/clients/client.redux.dev'));
-enrichServerSideRenderingHandler(server, reactReduxProvider);
+enrichClientBundleWithHotReloading(server, resolveDevStackPath('src/clients/client.redux'));
+enrichServerSideRenderingHandler(server, reactReduxProvider, htmlTemplate('bundle.js'));
 server.listen(3001);
