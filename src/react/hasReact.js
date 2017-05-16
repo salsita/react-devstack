1571d78f27f9022184ab1a6802f0b6ec5fd535b6
Tomas Weiss <tomasw@salsitasoft.com>
2017-05-16 14:12:38 +0200

    removed unnecessary check

--- src/react/hasReact.js
+++ src/react/hasReact.js
@@ -1,19 +1,8 @@
 import { existsSync } from 'fs';
 
-import { read, hasDependency } from '../utils/packageJson';
 import { resolveAppPath } from '../utils/pathResolvers';
 
-const packageJson = read(resolveAppPath('package.json'));
-
 export default () => {
-  if (!hasDependency(packageJson, 'react')) {
-    throw new Error('Please install react as a dependency: npm install react --save');
-  }
-
-  if (!hasDependency(packageJson, 'react-dom')) {
-    throw new Error('Please install react-dom as a dependency: npm install react-dom --save');
-  }
-
   if (!existsSync(resolveAppPath('src/components/Root.js'))) {
     throw new Error('Please create a root component in src/components/Root.js');
   }
